55626d38e5b5ab6445689ef01ea40b961f6d465f
caioriver <caio.rios@praxisjr.com.br>
2019-01-23 17:32:51 -0300

    Marcelo

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { /*Platform*/ StyleSheet, WebView, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
 import { FormLabel, FormInput, FormValidationMessage, Input } from 'react-native-elements';
@@ -31,13 +31,13 @@ export default class App extends Component {
   }
 
   login = async () => {
-    const { email , password } = this.state;
+    const { email, password } = this.state;
 
     try {
 
       const user = await firebase.auth()
-      .signInWithEmailAndPassword(email, password);
-      
+        .signInWithEmailAndPassword(email, password);
+
       this.setState({ isAuthenticated: true });
       console.log(user);
 
@@ -49,7 +49,7 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        
+
         <View style={styles.contentContainer}>
 
           <View style={styles.loginView}>
@@ -60,89 +60,93 @@ export default class App extends Component {
                   __DEV__
                     ? require('./assets/images/PersonalClub_Logo2-300x97.png')
                     : require('./assets/images/PersonalClub_Logo2-300x97.png')
-                  }
-              style={styles.welcomeImage}
+                }
+                style={styles.welcomeImage}
               />
             </View>
 
             <View style={styles.loginTitle}>
               <Text style={styles.getStartedText}>Login</Text>
             </View>
-          
-          <View style={styles.loginInput}>
 
-            {/* E-Mail */}
-            <FormInput 
-              leftIcon={
-                <Icon
-                  name="user-o"
-                  color="rgba(171, 189, 219, 1)"
-                  size={26}
-                />
-              }
-              placeholder="E-mail"
-              value={this.state.email}
-              containerStyle={{paddingVertical: 1, paddingHorizontal: 20}}
-              onChangeText={email => this.setState({ email })}
-              inputStyle={{ fontSize: 24, fontWeight: 'bold', color: 'white' }}
-              keyboardAppearance="light"
-              placeholder="E-mail"
-              autoCapitalize="none"
-              autoCorrect={false}
-              placeholderTextColor="white"
-              keyboardType="email-address"
-              returnKeyType="next"
-              ref={input => (this.emailInput = input)}
-              blurOnSubmit={false}
-              placeholderTextColor="#FFF"
-            />
-          </View>
-          
-          <View style={styles.loginInput}>
-          
-            {/* CPF */}
-            <FormInput
-              leftIcon={
-                <Icon
-                  name="lock"
-                  color="rgba(171, 189, 219, 1)"
-                  size={25}
-                />
-              }
-              value={this.state.password}
-              containerStyle={{paddingVertical: 1, paddingHorizontal: 20}}
-              onChangeText={password => this.setState({ password })}
-              inputStyle={{ fontSize: 24, fontWeight: 'bold', color: 'white' }}
-              secureTextEntry={false}
-              keyboardAppearance="light"
-              placeholder="CPF"
-              autoCapitalize="none"
-              autoCorrect={false}
-              keyboardType="default"
-              returnKeyType="done"
-              ref={input => (this.passwordInput = input)}
-              blurOnSubmit={true}
-              placeholderTextColor="white"
-            />
+            <View style={styles.loginInput}>
 
-          </View>
-          
+              <FormLabel>Name</FormLabel>
+              <FormInput onChangeText={someFunction} />
+              <FormValidationMessage>Error message</FormValidationMessage>
 
-          <View style={styles.helpContainer}>
+              {/* E-Mail */}
+              <FormInput
+                leftIcon={
+                  <Icon
+                    name="user-o"
+                    color="rgba(171, 189, 219, 1)"
+                    size={26}
+                  />
+                }
+                placeholder="E-mail"
+                value={this.state.email}
+                containerStyle={{ paddingVertical: 1, paddingHorizontal: 20 }}
+                onChangeText={email => this.setState({ email })}
+                inputStyle={{ fontSize: 24, fontWeight: 'bold', color: 'white' }}
+                keyboardAppearance="light"
+                placeholder="E-mail"
+                autoCapitalize="none"
+                autoCorrect={false}
+                placeholderTextColor="white"
+                keyboardType="email-address"
+                returnKeyType="next"
+                ref={input => (this.emailInput = input)}
+                blurOnSubmit={false}
+                placeholderTextColor="#FFF"
+              />
+            </View>
+
+            <View style={styles.loginInput}>
+
+              {/* CPF */}
+              <FormInput
+                leftIcon={
+                  <Icon
+                    name="lock"
+                    color="rgba(171, 189, 219, 1)"
+                    size={25}
+                  />
+                }
+                value={this.state.password}
+                containerStyle={{ paddingVertical: 1, paddingHorizontal: 20 }}
+                onChangeText={password => this.setState({ password })}
+                inputStyle={{ fontSize: 24, fontWeight: 'bold', color: 'white' }}
+                secureTextEntry={false}
+                keyboardAppearance="light"
+                placeholder="CPF"
+                autoCapitalize="none"
+                autoCorrect={false}
+                keyboardType="default"
+                returnKeyType="done"
+                ref={input => (this.passwordInput = input)}
+                blurOnSubmit={true}
+                placeholderTextColor="white"
+              />
+
+            </View>
+
+
+            <View style={styles.helpContainer}>
               <Text style={styles.termos}>Ao prosseguir você estará de acordo com nossos</Text>
-            <TouchableOpacity onPress={this._handleHelpPress}/** style={styles.helpLink}*/>
-              <Text style={styles.helpLinkText}>Termos de Uso e Política de Privacidade</Text>
-            </TouchableOpacity>            
-          </View>
-          
+              <TouchableOpacity onPress={this._handleHelpPress}/** style={styles.helpLink}*/>
+                <Text style={styles.helpLinkText}>Termos de Uso e Política de Privacidade</Text>
+              </TouchableOpacity>
+            </View>
 
-          <TouchableOpacity style={styles.button} onPress={this.login}>
-            <Text style={styles.buttonText}>Entrar</Text>
-          </TouchableOpacity>
 
-          { this.state.isAuthenticated ? <alert>Logado com sucesso</alert> : null }
+            <TouchableOpacity style={styles.button} onPress={this.login}>
+              <Text style={styles.buttonText}>Entrar</Text>
+            </TouchableOpacity>
 
-          {/* <Text style={styles.instructions}>{instructions}</Text> */}
+            {this.state.isAuthenticated ? <alert>Logado com sucesso</alert> : null}
+
+            {/* <Text style={styles.instructions}>{instructions}</Text> */}
 
           </View>
         </View>
@@ -155,7 +159,7 @@ export default class App extends Component {
       'https://www.google.com.br'
     );
   };
-  
+
 }
 
 const styles = StyleSheet.create({
@@ -173,7 +177,7 @@ const styles = StyleSheet.create({
   welcomeContainer: {
     alignItems: 'center',
     // marginTop: 10,
- 
+
   },
   welcomeImage: {
     resizeMode: 'contain',
